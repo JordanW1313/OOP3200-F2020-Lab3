8a61ccb92b314130aca79e02a563cc609760035e
JordanW1313 <jwriker@gmail.com>
2020-10-02 14:52:06 -0400

    Moved ExtendedWorkTicket defintion section to header and removed cpp file

--- OOP3200-F2020-Lab3/ExtendedWorkTicket.h
+++ OOP3200-F2020-Lab3/ExtendedWorkTicket.h
@@ -4,19 +4,17 @@
 #define _EXTENDED_WORKTICKET
 
 #include "WorkTicket.h"
+//#include "ExtendedWorkTicket.cpp"
 
 class ExtendedWorkTicket : public WorkTicket
 {
 public:
 
-	/*ExtendedWorkTicket() : WorkTicket()
-	}
-		SetTicketNumber(0);
-		SetClientId(" ");
-		SetDate(1, 1, 2020);
-		SetDescription(" ");
-		isOpen = true;
-	};*/
+	/*ExtendedWorkTicket(int myTicketNumber = 0, string myClientID = " ", int day = 1, int month = 1, int year = 2020, string myDescription = " ", bool myOpen = true) :
+		WorkTicket(myTicketNumber, myClientID, day, month, year, myDescription), isOpen{ myOpen } {
+
+	}*/
+
 	ExtendedWorkTicket(int myTicketNumber, string myClientID, int day, int month, int year, string myDescription, bool myOpen);
 
 	void CloseTicket();
@@ -26,9 +24,75 @@ public:
 
 	void ShowWorkTicket() const override;
 
+	friend ostream& operator<<(ostream& out, const ExtendedWorkTicket& ticket);
+
 private:
 	bool isOpen;
 };
 
+ExtendedWorkTicket::ExtendedWorkTicket(int myTicketNumber, string myClientID, int day, int month, int year, string myDescription, bool myOpen)
+{
+	SetTicketNumber(myTicketNumber);
+	SetClientId(myClientID);
+	SetDate(day, month, year);
+	SetDescription(myDescription);
+	isOpen = myOpen;
+}
+
+void ExtendedWorkTicket::CloseTicket()
+{
+	isOpen = false;
+}
+
+bool ExtendedWorkTicket::SetWorkTicket(int ticket_number, const string& client_id, int day, int month, int year, const string& description, bool myOpen)
+{
+	bool valid = true;
+
+	if (myOpen != true)
+	{
+		valid = false;
+	}
+	else
+	{
+		WorkTicket::SetWorkTicket(ticket_number, client_id, day, month, year, description);
+		isOpen = myOpen;
+	}
+
+	return valid;
+}
+
+
+void ExtendedWorkTicket::ShowWorkTicket() const
+{
+	WorkTicket::ShowWorkTicket();
+	if (isOpen == true)
+	{
+		cout << "Ticket: OPEN" << endl;
+	}
+	else
+	{
+		cout << "Ticket: CLOSED" << endl;
+	}
+
+}
+
+ostream& operator<<(ostream& out, const ExtendedWorkTicket& ticket)
+{
+	string valid;
+
+	if (ticket.isOpen == true)
+	{
+		valid = "OPEN";
+	}
+	else
+	{
+		valid = "CLOSED";
+	}
+
+	out << (WorkTicket)ticket
+		<< "Open:          " << valid << endl;
+
+	return out;
+}
 
 #endif
